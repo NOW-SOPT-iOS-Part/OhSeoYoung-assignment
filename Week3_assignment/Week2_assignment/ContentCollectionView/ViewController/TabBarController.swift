e6ac7c345825d50c29f6a66c9fa227bd32d9d9a9
Seoyoung Oh <ohseoyoung5@gmail.com>
2024-05-28 12:11:59 +0900

    Update .gitignore to exclude .DS_Store files

--- Week3_assignment/Week2_assignment/ContentCollectionView/ViewController/TabBarController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TabBarController.swift
-//  Week3_assignment
-//
-//  Created by 오서영 on 2024/05/03.
-//
-
-import Foundation
--- Week3_assignment/Week2_assignment/Extensions/UITextField+.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UITextField+.swift
-//  Week3_assignment
-//
-//  Created by 오서영 on 2024/05/03.
-//
-
-import Foundation
